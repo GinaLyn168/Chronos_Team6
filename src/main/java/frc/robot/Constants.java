609ce5c35dd42f32a864246eee6b49df63576a8e
Good Girl's Good Games <gracelin.tw88@gmail.com>
2024-11-12 20:26:40 -0800

    Fixed some things ig

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,5 +6,7 @@ public class Constants {
     public class Ports {
         public static final int shooterLeader = 13;
         public static final int shooterFollower = 12;
+
+        public static final int intakeM = 11;
     }
 }
