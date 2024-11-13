270f4a706260a692786d36b3b525aeba4b4f05aa
Max Lin <sky1381@outlook.com>
2024-11-12 20:03:06 -0800

    Changes to Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,5 @@
 package frc.robot;
 
 public class Constants {
-    
+    public static int IndexerMotor = 0;
 }
--- src/main/java/frc/robot/Subsystems/Indexer.java
+++ src/main/java/frc/robot/Subsystems/Indexer.java
@@ -1,13 +1,23 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import frc.robot.Constants;
+
 public class Indexer {
     //Max
     private static Indexer instance;
+
+    private TalonFX m_Indexer;
 
     public static Indexer getInstance() {
         if (instance == null) {
             instance = new Indexer();
         }
         return instance;
+    }
+
+    public void Indexer() {
+        m_Indexer = new TalonFX(Constants.IndexerMotor);
     }
 }
