4631e956b525bc3edc9d8ccfd5d06b96992a5ebf
Ryan Lin <solar138@outlook.com>
2024-11-12 19:58:07 -0800

    Added Phoenix6 TalonFX

--- src/main/java/frc/robot/Subsystems/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -1,5 +1,18 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 public class Shooter {
-    //Ryan
+    public TalonFX m_ShooterLeader;
+    public TalonFX m_ShooterFollower;
+
+    public static Shooter instance;
+
+    public static Shooter getInstance() {
+        if (instance == null) {
+            instance = new Shooter();
+        }
+
+        return instance;
+    }
 }
