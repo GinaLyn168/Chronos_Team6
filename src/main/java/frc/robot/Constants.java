3fb2372ac695cb2d1552e9523fd51e536c86c504
Max Lin <sky1381@outlook.com>
2024-11-12 20:31:52 -0800

    enum

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
 
     public class Ports {
         public static final int indexerMotor = 0;
+        
         public static final int shooterLeader = 13;
         public static final int shooterFollower = 12;
 
--- src/main/java/frc/robot/Subsystems/Indexer.java
+++ src/main/java/frc/robot/Subsystems/Indexer.java
@@ -1,14 +1,19 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class Indexer {
+public class Indexer extends SubsystemBase{
     //Max
     private static Indexer instance;
 
     private TalonFX m_Indexer;
+
+    private IndexerStates motorStates = IndexerStates.OFF;
 
     public static Indexer getInstance() {
         if (instance == null) {
@@ -19,5 +24,37 @@ public class Indexer {
 
     public void Indexer() {
         m_Indexer = new TalonFX(Constants.Ports.indexerMotor);
+        configMotor(m_Indexer);
+    }
+
+    public enum IndexerStates {
+        ON(0.25),
+        OFF(0),
+        REV(-0.25);
+
+        private double speed;
+
+        public double getSpeed() {
+            return speed;
+        }
+
+        IndexerStates(double speed) {
+            this.speed = speed;
+        }
+    }
+
+    private void configMotor(TalonFX motor) {
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+        motor.getConfigurator().apply(config);
+    }
+
+    public void setSpeed(double speed) {
+        m_Indexer.set(speed);
+    }
+
+    public void setSpeed(IndexerStates states) {
+        m_Indexer.set(states.getSpeed());
     }
 }
