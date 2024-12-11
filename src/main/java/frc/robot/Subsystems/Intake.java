4c991b387a8f20f3358622b5f9e44ee33c1f07fe
Ryan Lin <solar138@outlook.com>
2024-12-10 21:08:30 -0800

    Missing things Telemetry.java

--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import frc.robot.Constants;
 
